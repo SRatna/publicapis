ed5dcf7b8e6bdfe2ed08cafb650b9bf061b3f728
sratna <adhikariratna52@gmail.com>
2023-09-25 07:34:54 +0200

    enable simple UI layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import './App.css'
+import { Layout, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+import './App.css';
 
-function App() {
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <>
-      <h1>Public APIs</h1>
-    </>
-  )
-}
-
-export default App
+    <Layout className="layout">
+      <Header>
+        <div className="logo">
+          Public APIs
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Public APIs ©2023 Created by SRatna
+      </Footer>
+    </Layout>
+  );
+};
+export default App;
